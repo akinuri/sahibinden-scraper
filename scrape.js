a543c7af5beab087283e537622c6d5f4be762c0e
Noreh AD <akinoreh@gmail.com>
2026-05-02 21:39:55 +0300

    (feat) log execution time

--- scrape.js
+++ scrape.js
@@ -527,9 +527,12 @@ async function processesMapping(fieldsAndPaths) {
 console.clear();
 
 (async () => {
+    let swStart = performance.now();
     let info = await processesMapping(fieldsAndPaths);
     let json = JSON.stringify(info, null, 4);
 
     await copyToClipboard(json);
     console.log(json);
+    let swEnd = performance.now();
+    console.log(`Execution time: ${swEnd - swStart} ms`);
 })();
