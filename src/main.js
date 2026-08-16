77c10a30dc8ea855403cbd0dc979aad437fd9f65
Noreh AD <akinoreh@gmail.com>
2026-08-16 16:35:29 +0300

    (chore) main: intermediate console logs

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ const console = getCleanConsole();
 console.clear();
 
 (async () => {
+    console.log("Starting scraper...");
     let swStart = performance.now();
     let info = await processesMapping(fieldsAndPaths);
+    console.log("Stringifying ...");
     let json = JSON.stringify(info, null, 4);
 
     console.log(json);
     await copyToClipboard(json);
+    console.log("Copied to clipboard.");
     let swEnd = performance.now();
     console.log(`Execution time: ${swEnd - swStart} ms`);
 })();
