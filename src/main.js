e22ebd3a5a8e317f0d60f2a6b856bab82286c315
Noreh AD <akinoreh@gmail.com>
2026-08-16 16:03:29 +0300

    (refactor) log before copy

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ console.clear();
     let info = await processesMapping(fieldsAndPaths);
     let json = JSON.stringify(info, null, 4);
 
-    await copyToClipboard(json);
     console.log(json);
+    await copyToClipboard(json);
     let swEnd = performance.now();
     console.log(`Execution time: ${swEnd - swStart} ms`);
 })();
